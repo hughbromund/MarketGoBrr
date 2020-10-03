a759ff6de4d2438116944be2a9f2b66fbad6a1ad
Hugh Bromund <hughbromund44@gmail.com>
2020-10-03 17:47:57 -0400

    added github link and set primary color to our theme color

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
