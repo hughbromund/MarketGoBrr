6fd9286d321560b9339c47bb8eaa3620512b905f
Nathan Ashta <nathanashta@gmail.com>
2020-10-02 23:08:20 -0400

    Add routes to point to different components

---
+++ frontend/src/components/Analysis/Analysis.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Analysis extends Component {
+  render() {
+    return <div>Analysis</div>;
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Analysis from "./components/Analysis/Analysis";
 
 ReactDOM.render(
   <div>
     <NavigationBar />
     <Router>
       <div>
-        <Route path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/analysis" component={Analysis} />
+        <Route exact path="/" component={Home} />
       </div>
     </Router>
   </div>,
