5eecae8b0f3c60b2f75c243912a5cf400bce43f1
Aditya Naik <adityan9900@gmail.com>
2020-10-03 10:40:46 -0400

    Core functionality of tweet sentiment finished

--- sentiment/sentiment_analysis.py
+++ sentiment/sentiment_analysis.py
@@ -6,6 +6,7 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
+import twitter
 
 
 def print_result(annotations):
@@ -14,43 +15,56 @@ def print_result(annotations):
 
     for index, sentence in enumerate(annotations.sentences):
         sentence_sentiment = sentence.sentiment.score
-        print(
-            "Sentence {} has a sentiment score of {}".format(index, sentence_sentiment)
-        )
+        # print(
+        #     "Sentence {} has a sentiment score of {}".format(
+        #         index, sentence_sentiment)
+        # )
 
     print(
-        "Overall Sentiment: score of {} with magnitude of {}".format(score, magnitude)
+        "Overall Sentiment: score of {} with magnitude of {}".format(
+            score, magnitude)
     )
     return 0
 
 
-
-
-def analyze(movie_review_filename):
+def analyze(user_handle):
     """Run a sentiment analysis request on text within a passed filename."""
     client = language.LanguageServiceClient()
 
-    with open(movie_review_filename, "r") as review_file:
-        # Instantiates a plain text document.
-        content = review_file.read()
+    tweet_list = twitter.get_tweets(handle=user_handle)
 
-    document = types.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
-    annotations = client.analyze_sentiment(document=document)
+    for i in range(len(tweet_list)):
 
-    # Print the results
-    print_result(annotations)
+        content = tweet_list[i].get("text")
 
+        document = types.Document(
+            content=content, type=enums.Document.Type.PLAIN_TEXT)
+        annotations = client.analyze_sentiment(document=document)
 
+        # Print the results
+        # print_result(annotations)
+
+        score = annotations.document_sentiment.score
+        magnitude = annotations.document_sentiment.magnitude
+
+        tweet_list[i]["score"] = score
+        tweet_list[i]["magnitude"] = magnitude
+
+        print(tweet_list[i])
+
+    return tweet_list
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    parser.add_argument(
-        "movie_review_filename",
-        help="The filename of the movie review you'd like to analyze.",
-    )
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    # )
+    # parser.add_argument(
+    #     "movie_review_filename",
+    #     help="The filename of the movie review you'd like to analyze.",
+    # )
+    # args = parser.parse_args()
 
-    analyze(args.movie_review_filename)
+    # analyze(args.movie_review_filename)
+
+    analyze("realDonaldTrump")
