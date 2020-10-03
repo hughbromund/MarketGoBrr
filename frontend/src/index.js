a5f50643b6ed3accf260086bad4fb76f073ddcc3
Nathan Ashta <nathanashta@gmail.com>
2020-10-02 22:54:23 -0400

    Add navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
-const routing = (
-  <Router>
-    <div>
-      <Route path="/" component={App} />
-    </div>
-  </Router>
+ReactDOM.render(
+  <div>
+    <NavigationBar />
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+      </div>
+    </Router>
+  </div>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
