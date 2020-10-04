a94b0b8e4a7dca4bb7d65e91268b755c6272be39
Peyton Williams <peytonwilliams5@gmail.com>
2020-10-03 21:57:31 -0400

    database config files

--- backend/Database/Database.js
+++ backend/Database/Database.js
@@ -1,21 +1,22 @@
-
+const path = require("path");
+const secret = require(path.resolve(__dirname, "../secret.json"));
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE
-})
-
-/*
-    const path = require("path");
-    const secret = require(path.resolve(__dirname, "../secret.json"));
-
     host: secret.host,
     user: secret.user,
     password: secret.password,
     database: secret.database
+})
+
+/*
+   
+
+    
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
 */
 
 connection.connect()
